49518dd1136cc6384902ec0740f4deb9bf372f14
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-06-10 13:53:01 -0400

    Create roomType.js

---
+++ models/roomType.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config');
+
+class RoomType extends Model {}
+
+RoomType.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        maxOccupancy: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'roomType',
+    }
+);
+
+module.exports = RoomType;
