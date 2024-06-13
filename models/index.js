1bf20278e8bf5d78eec05c506e3d95e5e6dec6f7
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-06-12 20:59:35 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,26 +1,25 @@
-const { Guest, RoomType, Room, Reservation, ReservedRoom, OccupiedRoom } = require('./models');
+const { Guest } = require('./guest');
+const { RoomType } = require('./roomType');
+const { Room } = require('./room');
+const { Reservation } = require('./reservation');
+const { ReservedRoom } = require('./reservedRoom');
+const { OccupiedRoom } = require('./ocuppiedRoom');
 
-// Guest and Reservation
 Guest.hasMany(Reservation, { foreignKey: 'guestId' });
 Reservation.belongsTo(Guest, { foreignKey: 'guestId' });
 
-// RoomType and Room
 RoomType.hasMany(Room, { foreignKey: 'typeId' });
 Room.belongsTo(RoomType, { foreignKey: 'typeId' });
 
-// Reservation and ReservedRoom
 Reservation.hasMany(ReservedRoom, { foreignKey: 'reservationId' });
 ReservedRoom.belongsTo(Reservation, { foreignKey: 'reservationId' });
 
-// RoomType and ReservedRoom
 RoomType.hasMany(ReservedRoom, { foreignKey: 'roomTypeId' });
 ReservedRoom.belongsTo(RoomType, { foreignKey: 'roomTypeId' });
 
-// Room and OccupiedRoom
 Room.hasMany(OccupiedRoom, { foreignKey: 'roomId' });
 OccupiedRoom.belongsTo(Room, { foreignKey: 'roomId' });
 
-// Reservation and OccupiedRoom
 Reservation.hasMany(OccupiedRoom, { foreignKey: 'reservationId' });
 OccupiedRoom.belongsTo(Reservation, { foreignKey: 'reservationId' });
 
