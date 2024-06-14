9f9f48756cb2d19ec9afd2370ce0bcfd9129342b
kristyyip <kristyyip51998@gmail.com>
2024-06-13 21:26:05 -0400

    commented out api routes temporarily for deployment test

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 
-const guestRoutes = require('./guestRoutes');
-const resRoutes = require('./resRoutes');
-const roomRoutes = require('./roomRoutes');
-const roomTypeRoutes = require('./roomTypeRoutes');
-const occRoomRoutes = require('./occRoomRoutes');
-const resRoomRoutes = require('./resRoomRoutes');
+// const guestRoutes = require('./guestRoutes');
+// const resRoutes = require('./resRoutes');
+// const roomRoutes = require('./roomRoutes');
+// const roomTypeRoutes = require('./roomTypeRoutes');
+// const occRoomRoutes = require('./occRoomRoutes');
+// const resRoomRoutes = require('./resRoomRoutes');
 
-router.use('/guest', guestRoutes);
-router.use('/res', resRoutes);
-router.use('/room', roomRoutes);
-router.use('/roomType', roomTypeRoutes);
-router.use('/occRoom', occRoomRoutes);
-router.use('/resRoom', resRoomRoutes);
+// router.use('/guest', guestRoutes);
+// router.use('/res', resRoutes);
+// router.use('/room', roomRoutes);
+// router.use('/roomType', roomTypeRoutes);
+// router.use('/occRoom', occRoomRoutes);
+// router.use('/resRoom', resRoomRoutes);
 
 module.exports = router;
