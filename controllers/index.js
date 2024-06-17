246e7e6e1b3ad23fc1c939f518d9dab27e2b658e
kristyyip <kristyyip51998@gmail.com>
2024-06-17 12:59:37 -0400

    remove room type controller from index file

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const htmlController = require('./htmlController');
 const guestController = require('./guestController');
 const resController = require('./resController');
-const roomTypeController = require('./roomTypeController');
 const roomController = require('./roomController');
 
-module.exports = { htmlController, guestController, resController, roomTypeController, roomController };
+module.exports = { htmlController, guestController, resController, roomController };
